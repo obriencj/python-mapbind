3ff372902bd4e55b3574f8bb0c45a072d87781a8
Christopher O'Brien <obriencj@gmail.com>
2017-11-03 13:09:44 -0400

    getting there

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 
 
 """
-mapbind, a simple utility for binding vars from a map
+mapbind, assignment-aware binding functions
 
 author: Christopher O'Brien  <obriencj@gmail.com>
 license: LGPL v.3
@@ -30,7 +30,7 @@ except ImportError:
 
 
 setup(name = "mapbind",
-      version = "0.9.0",
+      version = "1.0.0",
 
       packages = (
           "mapbind",
@@ -44,12 +44,20 @@ setup(name = "mapbind",
       url = "https://github.com/obriencj/python-mapbind",
       license = "GNU Lesser General Public License v.3",
 
-      description = "Local bindings from a map",
+      description = "Assignment-aware binding functions",
+
+      # 2.6+ or 3.4+
+      python_requires = ">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
 
       classifiers = (
           "Intended Audience :: Developers",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
           "Topic :: Software Development",
       )
 )
